cdddeb7e78314ad0e8624e595af954e3e3f740d4
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-02-18 10:21:22 +0700

    update pertemuan 1

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description']; // Tambahkan atribut yang bisa diisi secara massal
 }
