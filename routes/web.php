0c1cee935a8ec6e796305cb5a0c5032064015b4b
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-02-21 20:23:59 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
-use App\Http\Controllers\UserProfileController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('welcome');
