34f100cc289e8d00a24e80b1e0ff9dc87146f1e4
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-02-23 20:34:34 +0700

    update view 4

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
     }
 
     public function greeting(){
-        return view('blog.hello', ['name' => 'Lovelyta Sekarayu']);
+        return view('blog.hello')
+            ->with('name', 'Andi')
+            ->with('occupation', 'Astronaut');
     }
 }
